f1ebe07a795a7af849bb9d25b93172fd52bb23ae
jeando <jean-do.favreau@hotmail.fr>
2013-12-05 12:11:57 +0100

    edp

---
+++ edp/diffusion_isotropique.m
@@ -0,0 +1,54 @@
+function diffusion_isotropique()
+
+    img=imread('LENA.BMP');
+    N=size(img,1);
+    img=double(img(:,:,1));
+    sigma_b = 7;
+    b=sigma_b * randn(size(img));
+    u0 = img + b;
+    u=u0;
+    K=500;
+    dt=.1;
+    
+    for k = 1:K
+    
+        grad_x = zeros(N,N);
+        grad_x(:,1:N-1) = u(:,2:N) - u(:,1:N-1);
+    
+        grad_y = zeros(N,N);
+        grad_y(1:N-1,:) = u(2:N,:) - u(1:N-1,:);
+
+        div_v = zeros(N,N);
+
+        div_v(:,1) = grad_x(:,1);
+        div_v(:,2:N-1) = grad_x(:,2:N-1) - grad_x(:,1:N-2);
+        div_v(:,N) = - grad_x(:,N-1);
+
+
+        div_v(1,:) = div_v(1,:) + grad_y(1,:);
+        div_v(2:N-1,:) = div_v(2:N-1,:) + grad_y(2:N-1,:) - grad_y(1:N-2,:);
+        div_v(N,:) = div_v(N,:) - grad_y(N-1,:);
+
+        u = u + dt * div_v;
+    end
+    
+    std = sqrt(2*K*dt);
+    Gsigma = fspecial('gaussian',N,std);%/(4*pi*K*dt);
+    sol2 = imfilter(u0,Gsigma,'symmetric');
+    disp(norm(u-sol2));
+    figure;imagesc(img/255);
+    colorbar;
+    colormap(gray);
+    figure;imagesc(u0/255);
+    colorbar;
+    colormap(gray);
+    figure;imagesc(u/255);
+    colorbar;
+    colormap(gray);
+    figure;imagesc(sol2/255);
+    colorbar;
+    colormap(gray);
+    figure;imagesc((u-sol2)/255);
+    colorbar;
+    colormap(gray);
+end
